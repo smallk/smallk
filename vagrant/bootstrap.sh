07979e842f0baf0fecb0a358ae5ef031ae8f8fd3
Stephen Lee-Urban <stephen.urban@gtri.gatech.edu>
2017-07-14 10:51:19 -0400

    in progress update of Vagrant release

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,29 +4,24 @@ date
 # Make sure the package information is up-to-date
 apt-get update
 
+mkdir -p /home/ubuntu/Downloads
+
 # Compilers
-apt-get install -y gcc-4.8
-apt-get install -y g++-4.8
-apt-get install -y gfortran-4.8
+apt-get install -y gcc-5
+apt-get install -y g++-5
+apt-get install -y gfortran-5
 apt-get install -y gfortran
 apt-get install -y unzip
 
 apt-get install -y python-dev
 apt-get install -y python-pip
+pip install --upgrade pip
 pip install numpy
 apt-get install -y python-scipy
 pip install cython
 
-# OpenMPI
-mkdir /home/vagrant/Downloads
-wget -O /home/vagrant/Downloads/openmpi-1.8.1.tar.gz http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.1.tar.gz
-tar -zxvf /home/vagrant/Downloads/openmpi-1.8.1.tar.gz -C /home/vagrant
-cd /home/vagrant/openmpi-1.8.1
-./configure --enable-mpi-thread-multiple --disable-dlopen --prefix=/usr/local  CC=/usr/bin/gcc-4.8 CXX=/usr/bin/g++-4.8 F77=usr/bin/gfortran-4.8 FC=/usr/bin/gfortran-4.8
-make -j4
-make install
-chown -R vagrant.vagrant /home/vagrant/Downloads
-chown -R vagrant.vagrant /home/vagrant/openmpi-1.8.1
+#mpich needed by Elemental 0.85
+apt-get install -y mpich
 
 # Source control
 apt-get install -y git
@@ -35,62 +30,41 @@ apt-get install -y git
 apt-get install -y cmake
 
 # libflame
-git clone https://github.com/flame/libflame.git /home/vagrant/libflame
-cd /home/vagrant/libflame
-./configure --prefix=/usr/local/flame --with-cc=/usr/bin/gcc-4.8 --with-ranlib=/usr/bin/gcc-ranlib-4.8
+git clone https://github.com/flame/libflame.git /home/ubuntu/libflame
+cd /home/ubuntu/libflame
+./configure --prefix=/usr/local/flame --with-cc=/usr/bin/gcc-5 --with-ranlib=/usr/bin/gcc-ranlib-5
 make -j4
 make install
-chown -R vagrant.vagrant /home/vagrant/libflame
+chown -R ubuntu /home/ubuntu/libflame
 
-# OpenBLAS
-wget -O /home/vagrant/Downloads/openblas-0.2.9.tar http://github.com/xianyi/OpenBLAS/tarball/v0.2.9
-tar -zxvf /home/vagrant/Downloads/openblas-0.2.9.tar -C /home/vagrant
-cd /home/vagrant/xianyi-OpenBLAS-347dded
-make BINARY=64 USE_OPENMP=1
-make PREFIX=/usr/local/openblas/0.2.9/ install
-ln -s /usr/local/openblas/0.2.9/lib/libopenblas.so /usr/local/lib/libopenblas.so
-ln -s /usr/local/openblas/0.2.9/lib/libopenblas.so.0 /usr/local/lib/libopenblas.so.0
-chown -R vagrant.vagrant /home/vagrant/xianyi-OpenBLAS-347dded
-
+# OpenBLAS and LAPACK
+apt-get install -y libopenblas-dev
+apt-get install -y libatlas-dev liblapack-dev
 
 # Elemental
 mkdir /usr/local/elemental
 export ELEMENTAL_INSTALL_DIR=/usr/local/elemental
-wget -O /home/vagrant/Downloads/Elemental-0.84-p1.tgz http://libelemental.org/pub/releases/Elemental-0.84-p1.tgz
-tar -zxvf /home/vagrant/Downloads/Elemental-0.84-p1.tgz -C /home/vagrant
-cd /home/vagrant/Elemental-0.84-p1
+wget -O /home/ubuntu/Downloads/Elemental-0.85.tgz http://libelemental.org/pub/releases/Elemental-0.85.tgz
+tar -zxvf /home/ubuntu/Downloads/Elemental-0.85.tgz -C /home/ubuntu
+cd /home/ubuntu/Elemental-0.85
+
+#Version 0.85 of Elemental has an error in one of its cmake files. The file is: 
+#	Elemental-0.85/cmake/tests/CXX.cmake
+sed -i'' '1s/include(FindCXXFeatures)/include_directories(FindCXXFeatures)/' ./cmake/tests/CXX.cmake
+
 mkdir build_hybrid
 mkdir build_pure
+
 cd build_hybrid
-cmake -D CMAKE_INSTALL_PREFIX=/usr/local/elemental/0.84-p1/HybridRelease -D CMAKE_BUILD_TYPE=HybridRelease -D CMAKE_CXX_COMPILER=/usr/bin/g++-4.8 -D CMAKE_CXX_FLAGS=-fPIC -D CMAKE_C_COMPILER=/usr/bin/gcc-4.8 -D CMAKE_C_FLAG=-fPIC -D CXX_FLAGS="-std=c++11 -fPIC" -D CMAKE_Fortran_COMPILER=/usr/bin/gfortran-4.8 -D MATH_LIBS="/usr/local/flame/lib/libflame.a;-L/usr/local/lib/ -lopenblas;/lib/x86_64-linux-gnu/libm.so.6" –D ELEM_EXAMPLES=ON –D ELEM_TESTS=ON  ..
+cmake -D CMAKE_INSTALL_PREFIX=/usr/local/elemental/0.85/HybridRelease -D CMAKE_BUILD_TYPE=HybridRelease -D CMAKE_CXX_COMPILER=/usr/bin/g++-5 -D CMAKE_CXX_FLAGS="-std=c++11 -fPIC" -D CMAKE_C_COMPILER=/usr/bin/gcc-5 -D CMAKE_C_FLAG=-fPIC -D CXX_FLAGS="-std=c++11 -fPIC" -D CMAKE_Fortran_COMPILER=/usr/bin/gfortran-5 -D MATH_LIBS="/usr/local/flame/lib/libflame.a;-L/usr/local/lib/ -lopenblas;/lib/x86_64-linux-gnu/libm.so.6" –D ELEM_EXAMPLES=ON –D ELEM_TESTS=ON  ..
 make -j4
 make install
 
 cd ../build_pure
-cmake -D CMAKE_INSTALL_PREFIX=/usr/local/elemental/0.84-p1/PureRelease -D CMAKE_BUILD_TYPE=PureRelease -D CMAKE_CXX_COMPILER=/usr/bin/g++-4.8 -D CMAKE_CXX_FLAGS=-fPIC -D CMAKE_C_COMPILER=/usr/bin/gcc-4.8 -D CMAKE_C_FLAG=-fPIC -D CXX_FLAGS="-std=c++11 -fPIC" -D CMAKE_Fortran_COMPILER=/usr/bin/gfortran-4.8 -D MATH_LIBS="/usr/local/flame/lib/libflame.a;-L/usr/local/lib/ -lopenblas;/lib/x86_64-linux-gnu/libm.so.6" –D ELEM_EXAMPLES=ON –D ELEM_TESTS=ON  ..
+cmake -D CMAKE_INSTALL_PREFIX=/usr/local/elemental/0.85/PureRelease -D CMAKE_BUILD_TYPE=PureRelease -D CMAKE_CXX_COMPILER=/usr/bin/g++-5 -D CMAKE_CXX_FLAGS="-std=c++11 -fPIC" -D CMAKE_C_COMPILER=/usr/bin/gcc-5 -D CMAKE_C_FLAG=-fPIC -D CXX_FLAGS="-std=c++11 -fPIC" -D CMAKE_Fortran_COMPILER=/usr/bin/gfortran-5 -D MATH_LIBS="/usr/local/flame/lib/libflame.a;-L/usr/local/lib/ -lopenblas;/lib/x86_64-linux-gnu/libm.so.6" –D ELEM_EXAMPLES=ON –D ELEM_TESTS=ON  ..
 make -j4
 make install
-chown -R vagrant.vagrant /home/vagrant/Elemental-0.84-p1
 
-# Java
-cp /vagrant/jdk-8u25-linux-x64.gz /home/vagrant/Downloads
-tar -zxvf /home/vagrant/Downloads/jdk-8u25-linux-x64.gz -C /home/vagrant
-chown -R vagrant.vagrant /home/vagrant/jdk1.8.0_25
+#chown -R vagrant.vagrant /home/ubuntu/Elemental-0.84-p1
 
-#somehow add to .bashrc
-echo "export JAVA_HOME=/home/vagrant/jdk1.8.0_25/jre" >> .bashrc
-apt-get install -y ant
-ln -s /home/vagrant/jdk1.8.0_25/jre/bin/java /usr/local/bin/java
 
-# SmallK
-cp /vagrant/libsmallk-1.6.0.tar.gz /home/vagrant/
-#cp /vagrant/smallk_data.zip /home/vagrant/ ##### sink this directory with
-tar -zxvf /home/vagrant/libsmallk-1.6.0.tar.gz -C /home/vagrant
-#unzip /home/vagrant/smallk_data.zip -d /home/vagrant/smallk_data
-cd /home/vagrant/libsmallk-1.6.0
-
-#make PYSMALLK=1 JAVA=/usr/local/bin/java ANT=/usr/bin/ant
-# make SITE_PACKAGES_DIR=/usr/local/lib/python2.7/dist-packages/ install
-make all DATA_DIR=../smallk_data
-#make check
-chown -R vagrant.vagrant /home/vagrant/libsmallk-1.6.0
